ed120f5e803003cf515a6e6ccd55ee3082c4ee52
Diego-EC <diegoezquerro@gmail.com>
2020-09-30 08:00:33 +0000

    finished
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- exercises/22-Bottles-Of-Milk/app.py
+++ exercises/22-Bottles-Of-Milk/app.py
@@ -1,8 +1,15 @@
 def number_of_bottles():
     number_of_bottles = 99
-    for x in reversed(number_of_bottles):
-        string = x + " bottles of milk on the wall, " + x + " bottles of milk."
-        print(string)
+    #range(5,0,-1)
+    for x in range(99,0,-1):
+        if x == 1:
+            sentence_1 = str(x) + " bottle of milk on the wall, " + str(x) + " bottle of milk.\n" + "Take one down and pass it around, no more bottles of milk on the wall."
+            sentence_2 = "No more bottles of milk on the wall, no more bottles of milk.\n" + "Go to the store and buy some more, 99 bottles of milk on the wall."
+        else:
+            sentence_1 = str(x) + " bottles of milk on the wall, " + str(x) + " bottles of milk."
+            sentence_2 = "Take one down and pass it around, " +  str(x -1) + " bottles of milk on the wall."
+        print(sentence_1)
+        print(sentence_2)
 
 
 number_of_bottles()
